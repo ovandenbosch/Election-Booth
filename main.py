bdc1c9287bc23f8e6e4768f4a1c9ccaf02495f99
Oliver van den Bosch <vanders06@icloud.com>
2021-03-22 12:26:39 -0400

    Update for validation

--- main.py
+++ main.py
@@ -9,20 +9,28 @@ def vote():
     can_b = 0
     can_c = 0
     vote = ""
+    letters = ['a', 'b', 'c']
+
 
     # Checking if END keyword has not been called
-    while vote != "END":
+    
+    
+
+    while vote != "end":
         vote = input("Which candidate would you like to vote for, A, B or C? ")
-        vote1 = vote.lower()
+        print('\n')
+        vote = vote.lower()
+        if vote == 'end':
+            break
         # User validation
-        while vote1 != "a" or "b" or "c":
-            print(vote)
-            vote = input("Please enter A, B or C? ")
+        while vote not in letters:
+            vote = input("You did not enter a valid amount. Please enter A, B or C? ")
+            print('\n')
         
             
         # Adds votes
         if vote.lower() == "a":
-                can_a += 1
+                can_a += 1   
         elif vote.lower() == "b":
                 can_b += 1
         elif vote.lower() == "c":
