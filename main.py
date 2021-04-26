7a7bebdec92723138ed362135794e7a66fee0848
Oliver van den Bosch <>
2021-04-26 20:02:00 +0000

    Add corruption method

--- main.py
+++ main.py
@@ -4,17 +4,18 @@
 
 
 def vote():
-    
+
     # Amount of votes per candidate at beginning
     can_a = 0
     can_b = 0
     can_c = 0
+    # We need to set the initial value because if system is corrupted, value will change to 100,000
+    value = 1
     vote = ""
-    letters = ['a', 'b', 'c']
-
+    letters = ['a', 'b', 'c', 'corrupt']
 
     # Checking if END keyword has not been called
-    
+
     while vote != "end":
         # Getting input
         vote = input("Which candidate would you like to vote for, A, B or C? ")
@@ -25,21 +26,29 @@ def vote():
         if vote == 'end':
             ## exits the while loop and goes to the print statements below
             break
-            
+
+        # Handling what happens if someone corrupts the system
+        if vote == "corrupt":
+            # Setting the vote to 100,000
+            value = 100000
+            vote = input(
+                "You have corrupted the system you naughty person! Choose who you want to give 100,000 votes to: "
+            )
+
         # User validation cross checking against list
         while vote not in letters:
-            vote = input("You did not enter a valid amount. Please enter A, B or C? ")
+            vote = input(
+                "You did not enter a valid amount. Please enter A, B or C? ")
             print('\n')
-        
-            
+
         # Adds votes
         if vote.lower() == "a":
-                can_a += 1   
+            can_a += value
         elif vote.lower() == "b":
-                can_b += 1
+            can_b += value
         elif vote.lower() == "c":
-                can_c += 1
-    
+            can_c += value
+
     # Printing votes
     totalvotes = can_a + can_b + can_c
     print(f"Candidate A has {can_a} votes.")
@@ -47,6 +56,7 @@ def vote():
     print(f"Candidate C has {can_c} votes.")
     print(f'Total votes: {totalvotes}')
 
+    # Asking the user if they would like to start the system again after calling end.
     askagain = input('Would you like to go again, [y,n]')
     if askagain == 'y':
         vote()
