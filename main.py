d1aba323213dd3bc66c74c6aaaba3095fa07be93
Oliver van den Bosch <vanders06@icloud.com>
2021-04-27 14:46:38 +0100

    Start creating function

--- main.py
+++ main.py
@@ -3,16 +3,20 @@
 # Voting system
 
 
+
+
 def vote():
 
     # Amount of votes per candidate at beginning
     can_a = 0
     can_b = 0
     can_c = 0
+    
     # We need to set the initial value because if system is corrupted, value will change to 100,000
     value = 1
     vote = ""
     letters = ['a', 'b', 'c', 'corrupt']
+    overall = [0, 0, 0]
 
     # Checking if END keyword has not been called
 
@@ -26,6 +30,11 @@ def vote():
 
         # Checking if end keyword again
         if vote == 'end':
+
+            
+            
+            
+            print(f"Overall is {overall}")
             ## exits the while loop and goes to the print statements below
             break
 
@@ -51,6 +60,8 @@ def vote():
             can_b += value
         elif vote.lower() == "c":
             can_c += value
+        
+        overall = [can_a, can_b, can_c]
 
     # Printing votes
     totalvotes = can_a + can_b + can_c
@@ -59,11 +70,22 @@ def vote():
     print(f"Candidate B has {can_b} votes.")
     print(f"Candidate C has {can_c} votes.")
     print(f'Total votes: {totalvotes}')
+    print(f"Overall is {overall}")
 
     # Asking the user if they would like to start the system again after calling end.
     askagain = input('Would you like to go again, [y,n]')
     if askagain == 'y':
         vote()
+
+
+
+def save(overall):
+    with open("votes.txt", "w") as voteFile:
+        for voteitem in overall:
+            voteFile.write(f"{voteitem}, \n")
+
+def print(overall):
+    pass
 
 
 # Calling function and stopping if any errors
@@ -76,3 +98,7 @@ if __name__ == '__main__':
         print('Runtime Error')
     finally:
         print("Have a good day")
+
+
+
+
